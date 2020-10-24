bce0da8372e0185ea9cf0b595550c16d47f9a939
Ulan <ulan.chokushov@gmail.com>
2020-10-24 11:08:56 +0600

    first commit

--- script.js
+++ script.js
@@ -23,4 +23,26 @@
 
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы посмотрели?');
+const  numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?'),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
